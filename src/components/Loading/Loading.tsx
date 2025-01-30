efc3de97eb49794003bef29f6137fbb3378260da
Lalo Blanc <lalo@physicslab.space>
2025-01-30 14:21:18 -0800

    update Loading component background color from emerald to cyan and change Load variant to light

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -7,8 +7,8 @@ import Load from "../Load/Load";
 
 const Loading: React.FC = () => {
   return (
-    <section className="flex justify-center items-center h-screen bg-emerald-800">
-      <Load />
+    <section className="flex justify-center items-center h-screen bg-cyan-500">
+      <Load variant="light"/>
     </section>
   );
 };
