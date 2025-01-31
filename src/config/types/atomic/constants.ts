13693038e8242f539272300892dd5832527e2a56
Lalo Blanc <lalo@physicslab.space>
2025-01-31 15:02:32 -0800

    update fundamental particle type colors for improved visibility

--- src/config/types/atomic/constants.ts
+++ src/config/types/atomic/constants.ts
@@ -16,8 +16,8 @@ export const elementCategoryColors: Record<string, string> = {
 
 
 export const fundamentalParticleTypeColors: Record<FundamentalParticleType, string> = {
-    [FundamentalParticleType.QUARK]: "bg-sky-50 border-sky-500 text-sky-500",
-    [FundamentalParticleType.LEPTON]: "bg-lime-50 border-lime-500 text-lime-500",
-    [FundamentalParticleType.BOSON]: "bg-red-50 border-red-500 text-red-500",
-    [FundamentalParticleType.HIGGS]: "bg-yellow-50 border-yellow-500 text-yellow-500",
+    [FundamentalParticleType.QUARK]: "bg-sky-100 border-sky-500 text-sky-500",
+    [FundamentalParticleType.LEPTON]: "bg-lime-100 border-lime-500 text-lime-500",
+    [FundamentalParticleType.BOSON]: "bg-red-100 border-red-500 text-red-500",
+    [FundamentalParticleType.HIGGS]: "bg-yellow-100 border-yellow-500 text-yellow-500",
   };
